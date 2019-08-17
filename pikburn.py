c650966618be4ed77919c5c8837ead1093285860
l337quez <l337.ronald@gmail.com>
2019-08-16 20:57:19 -0400

    update

--- pikburn.py
+++ pikburn.py
@@ -1,39 +1,59 @@
 #!/usr/bin/python
 #Importamos las clases de PySide
+
+#revisar https://github.com/shuge/Qt-Python-Binding-Examples/tree/master/common_widgets
+
 import sys
-from PySide import QtCore,QtGui
-
-
-# #Creamos una aplicacion Qt
-app = QtGui.QApplication(sys.argv)
-mainwindow= QtGui.QWidget()
-mainwindow.resize(400,350)
-mainwindow.setWindowTitle("PikBurn")
-mainwindow.show()
-
-sys.exit(app.exec_())
+try:
+    from PySide import QtCore
+    from PySide import QtGui
+except ImportError:
+    from PyQt4 import QtCore
+    from PyQt4 import QtGui
 
 
 
 
+class Demo(QtGui.QWidget):
+    def __init__(self):
+        super(Demo, self).__init__()
+
+        x, y, w, h = 500, 200, 300, 400
+        self.setGeometry(x, y, w, h)
+
+
+        combo = QtGui.QComboBox(self)
+        combo.move(20, 20)
+
+        combo.currentIndexChanged.connect(self._cb_currentIndexChanged)
+        combo.highlighted.connect(self._cb_highlighted)
+
+        items = ('', 'Lisp', 'C', 'Objective-C', 'Python', 'Java')
+        combo.addItems(items)
+
+    def _cb_currentIndexChanged(self, idx):
+        print ('current selected index:', idx)
+
+    def _cb_highlighted(self, idx):
+        print ('highlighted index:', idx)
+
+    def show_and_raise(self):
+        self.show()
+        self.raise_()
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+
 
 
 
+    demo = Demo()
+    # Tamaño de la ventana
+    demo.resize(400,350)
+    #NOmbre de la ventana
+    demo.setWindowTitle("PikBurn")
+    demo.show_and_raise()
 
 
-
-
-#!/usr/bin/python
-
-# Importamos las clases de PySide
-# import sys
-# from PySide.QtCore import *
-# from PySide.QtGui import *
-
-# #Creamos una aplicacion Qt
-# app = QApplication(sys.argv)
-# #Creamos una etiqueta y la mostramos
-# label = QLabel("hola")
-# label.show()
-# app.exec_()
-# sys.exit()
+    sys.exit(app.exec_())
